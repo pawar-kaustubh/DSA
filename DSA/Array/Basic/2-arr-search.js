d016853779527297f31e568721037a616562fc0c
Kaustubh Pawar <167971412+pawar-kaustubh@users.noreply.github.com>
2025-12-21 10:44:52 +0530

    search the number in the array and return its index

---
+++ DSA/Array/Basic/2-arr-search.js
@@ -0,0 +1,13 @@
+function arrSearch(arr, x) {
+  // convert  x to number
+  x = Number(x);
+
+  // Loop through the array
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === x) return i;
+  }
+  return -1; // return -1 if x is not found
+}
+
+console.log(arrSearch([2, 3, 5, 7, 8], 5));
+console.log(arrSearch([2, 3, 5, 7, 8], 6));

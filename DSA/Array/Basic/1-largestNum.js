7f9d81085e5d68097feb8b3dfdef0d169947a043
Kaustubh Pawar <167971412+pawar-kaustubh@users.noreply.github.com>
2025-12-21 10:41:22 +0530

    find largest element in array

---
+++ DSA/Array/Basic/1-largestNum.js
@@ -0,0 +1,13 @@
+// Questions are include in the Basic.md files.
+
+function largestNumber(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > num) {
+      max = arr[i];
+    }
+  }
+  return num;
+}
+
+console.log(largestNumber([1, 23, 24, 1, 3, 4]));
---
+++ DSA/Array/Basic/Basic.md
@@ -0,0 +1,11 @@
+# Q 1. Given an array arr[]. The task is to find the largest element and return it.
+
+## Examples:
+- Input: arr[] = [1, 8, 7, 56, 90]
+Output: 90
+Explanation: The largest element of the given array is 90.
+
+Constraints:
+1 <= arr.size()<= 106
+0 <= arr[i] <= 106
+

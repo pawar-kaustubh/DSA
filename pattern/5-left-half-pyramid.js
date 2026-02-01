a28ee84cfa70cea67a6099967883d03cc7325e74
Kaustubh Pawar <167971412+pawar-kaustubh@users.noreply.github.com>
2026-02-01 11:31:38 +0530

    print left half triangle

---
+++ pattern/5-left-half-pyramid.js
@@ -0,0 +1,13 @@
+function leftPyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n - i; j++) {
+      row += " ";
+    }
+    for (let k = 1; k <= i; k++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+leftPyramid(5);
